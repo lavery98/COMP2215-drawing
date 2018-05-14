b8e943fe4f786393a6abd5056bb001f6d310de21
Ashley <ashley@sa-irc.com>
2018-05-14 14:19:39 +0100

    Uncomment demo code

--- sample_main.c
+++ sample_main.c
@@ -8,7 +8,7 @@ void main(void) {
 
   init_lcd();
 
-  /*stroke_rectangle(0, 0, 100, 100, BLUE);
+  stroke_rectangle(0, 0, 100, 100, BLUE);
 
   fill_circle(50, 50, 60, RED);
   stroke_circle(50, 50, 60, GREEN);
@@ -19,7 +19,7 @@ void main(void) {
 
   stroke_line(200, 100, 0, 0, WHITE);
 
-  fill_rounded_rectangle(100, 100, 100, 100, 20, BLUE);*/
+  fill_rounded_rectangle(100, 100, 100, 100, 20, BLUE);
 
   uint16_t xPoints[] = {25, 25, 50, 50, 125, 125, 50, 50, 125, 125, 25};
   uint16_t yPoints[] = {25, 175, 175, 100, 100, 75, 75, 50, 50, 25, 25};
